45d9ec1a5306081cd784995323e579b0dccd2d85
TomiJolkkonen <tomi.jolkkonen@skillio.ai>
2025-01-15 16:26:58 +0200

    main.py tehty osan 2 tehtäviä 4 ja 5

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import users
-import product_catalogue
 import transactions
-
+import pandas as pd
 
 def main():
-    print(users.get_users())
-    print(products.get_product_catalog())
-    print(transactions.get_transactions())
+    users, products, transaction_data=transactions.get_transactions()
+    print(users)
+    print(products)
+    print(transaction_data)
+    
+    # total_spending = transaction_data.groupby('user_id')['quantity'].sum() # total spending per user
+    # print(f"Total spending per user: {total_spending}")
+    # top5_sold = products.groupby(['Product ID']).agg(total_quantity_sold=('Price'), average_price=('Price')) # top 5 best-selling products & their avr price
+    # most_popular = products.groupby('Category')['quantity'].sum() # most popular product category
+    transaction_data.to_json('transaction_data.json', orient='records', lines=True) # export dataset to json file
+    df_from_json = pd.read_json('transaction_data.json', lines=True) # read json file back into pandas dataframe
+    print(f"First few lines from the file: {df_from_json.head()}") # print the head of the file out
 
 if __name__ == "__main__":
     main()
