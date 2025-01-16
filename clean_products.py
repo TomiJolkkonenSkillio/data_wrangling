33a8ee11407df844896c62a200c5e8fbd9f9a4ee
Leija Salmivirta <leija.salmivirta@skillio.ai>
2025-01-16 14:21:41 +0200

    Added methods to clean data

--- clean_products.py
+++ clean_products.py
@@ -7,12 +7,13 @@ import pandas as pd
 def read_products_data():
     file_path='product_catalog_with_rubbish.csv'
     df=pd.read_csv(file_path)
+    print(df.dtypes)
     return df
 
 
 def get_data_description(df):
     description= df.describe()
-    print(description)
+    #print(description)
 
 def convert_price(price):
     try:
@@ -20,45 +21,39 @@ def convert_price(price):
     except (ValueError, TypeError):
         return np.nan
     
-def convert_category(category):
-    pattern=r'^[\w\s]+$'
-    if isinstance(category, str):
-        if re.match(pattern, category):
-            return category.strip()  # Clean and return if valid
+def convert_category(column):
+    pattern = r'^[a-zA-Z\s]+$'
+    if isinstance(column, str):
+        if re.match(pattern, column):
+            return column.strip()  # Clean and return if valid
         else:
             return np.nan 
-    elif pd.isna(category):  # Explicitly check for NaN
+    elif pd.isna(column):  # Explicitly check for NaN
         return np.nan
     else:
         return np.nan 
    
+def handle_wrong_datatype_in_id(df):
+    df['Product ID']=pd.to_numeric(df['Product ID'], errors='coerce')
 
 def handle_wrong_datatype_in_price(df):
-    df['Price']=df['Price'].apply(convert_price)
-
-    """
     df['Price'] = pd.to_numeric(df['Price'], errors='coerce')
-    return df
-    """
 
-def handle_wrong_datatype_in_category(df):
-    df['Category']=df['Category'].apply(convert_category)
+def handle_wrong_datatype_in_name_and_category(df,column:str):
+    df[column]=df[column].apply(convert_category)
 
 def handle_dublicate_values_in_id(df):
-    pass
-
+    df['Product ID'] = df['Product ID'].drop_duplicates()
 
 def get_number_of_missing_values(df):
     missing_values=df.isna().sum()
-    print(missing_values)
 
 
 def main():
     dataframe=read_products_data()
-    get_data_description(dataframe)
+    handle_wrong_datatype_in_id(dataframe)
     handle_wrong_datatype_in_price(dataframe)
-    get_number_of_missing_values(dataframe)
-    handle_wrong_datatype_in_category(dataframe)
+    handle_wrong_datatype_in_name_and_category(dataframe, 'Category')
     handle_dublicate_values_in_id(dataframe)
     print(dataframe)
 
